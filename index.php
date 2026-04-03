6c83018212c1fdd870d586c6f996fb68f2a2c1da
DjibrilMTRAORE <traodjibril.250@gmail.com>
2026-04-03 02:28:40 +0000

    DevSecOps Actions

--- index.php
+++ index.php
@@ -1,35 +1,44 @@
 <?php
+
 $valid_user = "admin";
-$valid_pass = password_hash("password", PASSWORD_BCRYPT);
+$valid_pass = "password";
 
 $message = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $user = htmlspecialchars($_POST["username"]);
+
+    $user = $_POST["username"];
     $pass = $_POST["password"];
 
-    if ($user === $valid_user && password_verify($pass, $valid_pass)) {
-        $message = "Connexion réussie ✅";
+    if ($user == $valid_user && $pass == $valid_pass) {
+
+        $message = "Bienvenue " . $user . " 🎉";
+
+        $message .= "<br>Connexion réussie";
     } else {
-        $message = "Identifiants incorrects ";
+
+        $message = "Échec login pour : " . $user;
     }
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Secure Login</title>
+    <title>Vulnerable Login LAB</title>
 </head>
 <body>
-    <h2>Login sécurisé</h2>
 
-    <form method="POST">
-        <input type="text" name="username" placeholder="Username" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <button type="submit">Login</button>
-    </form>
+<h2>Login (VULNÉRABLE - LAB)</h2>
 
-    <p><?php echo $message; ?></p>
+<form method="POST">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <button type="submit">Login</button>
+</form>
+
+<p><?php echo $message; ?></p>
+
 </body>
 </html>
